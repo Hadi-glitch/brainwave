a20ec0e823ce0693f7af3224930ff510bccd8861
Hadi <abdulhadi74737@gmail.com>
2024-10-23 13:51:33 +0500

    first commit

---
+++ app/(auth)/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const layout = ({children}) => {
+  return (
+    <main className="flex items-center justify-center h-[100vh]">{children}</main>
+  )
+}
+
+export default layout
---
+++ app/(root)/generation/page.jsx
@@ -0,0 +1,12 @@
+import Section from '@/components/Section'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Section>
+      Generation
+    </Section>
+  )
+}
+
+export default page
